6a6fbea266210106ce96aa014786d3d89cb730de
ACullen97 <alex_cullen123@hotmail.co.uk>
2025-06-09 13:44:48 +0100

    Added api.js to events fe

---
+++ events-fe/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+export const getEvents = () => {
+
+    return axios.get(`https://launchpad-frontend-dcao.onrender.com/api/event/list`).then(({data}) => {
+        return data;
+    })
+}
+
+export const getEvent = (id) => {
+
+    return axios.get(`https://launchpad-frontend-dcao.onrender.com/api/event/${id}`).then(({data}) => {
+        return data
+    })
+}
+
+
+
+export default getEvents
